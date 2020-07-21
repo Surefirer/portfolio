6378b1e715bd314e755aced49af4683d3b36cb3c
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-07-20 19:03:25 -0700

    add process.evn.public-url

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL} y>
         <div style={{ height: "100%" }}>
           <Header drawerClickHandler={this.drawerToggleClickHandler} />
           <SideDrawer
